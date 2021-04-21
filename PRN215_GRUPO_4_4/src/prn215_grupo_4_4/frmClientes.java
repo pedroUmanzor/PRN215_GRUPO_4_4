dbfd2522de8c517c7425711562142d149c704914
Oscar Leonel Segura Valencia <SV20008@ti.ues.edu.sv>
2021-04-20 22:01:18 -0600

    Aguegue formulario de clientess

--- PRN215_GRUPO_4_4/src/prn215_grupo_4_4/frmClientes.java
+++ PRN215_GRUPO_4_4/src/prn215_grupo_4_4/frmClientes.java
@@ -28,10 +28,13 @@ public class frmClientes extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("Registro de Clientes.");
+
+        jLabel2.setText("hola");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -41,13 +44,19 @@ public class frmClientes extends javax.swing.JFrame {
                 .addContainerGap(113, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(170, 170, 170))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(129, 129, 129)
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(45, 45, 45)
                 .addComponent(jLabel1)
-                .addContainerGap(239, Short.MAX_VALUE))
+                .addGap(51, 51, 51)
+                .addComponent(jLabel2)
+                .addContainerGap(172, Short.MAX_VALUE))
         );
 
         pack();
@@ -90,5 +99,6 @@ public class frmClientes extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
